a735ecd3a53101017f922b76e5cb497aeace4a89
Alex <wynterblue02@gmail.com>
2023-08-23 15:01:45 -0400

    fixed?

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState([])
 
   useEffect(() => {
-    fetch('https://starter-name-generator-client-sbyb.onrender.com/generate')
+    fetch('https://starter-name-generator-alar.onrender.com/generate')
       .then((res) => res.json())
       .then((nme) => setName([nme]))
   }, [])
